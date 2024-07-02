21cd1d2e84a5ff2c7c061e988db8d6a6c05c359b
ivanpfaab <pfaabivan@gmail.com>
2024-07-02 20:11:56 -0300

    Adapting header

--- fronted-assignment-service/src/components/Header.js
+++ fronted-assignment-service/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Container, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
+import fiubaLogo from '../assets/Logo-fiuba_big_face.png'; // Asegúrate de que el path al logo es correcto
 const Header = ({ user }) => {
   const navigate = useNavigate();
 
@@ -9,13 +9,21 @@ const Header = ({ user }) => {
     <AppBar position="static">
       <Container maxWidth="lg">
         <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            FIUBA App
-          </Typography>
+          <Box display="flex" alignItems="center" flexGrow={1}>
+            <img src={fiubaLogo} alt="FIUBA Logo" style={{ height: '40px', marginRight: '15px' }} />
+            <Typography variant="h6">
+              FIUBA
+            </Typography>
+          </Box>
           {user && (
-            <Button color="inherit" onClick={() => navigate('/profile')}>
-              Ver Perfil
-            </Button>
+            <Box>
+              <Button color="inherit" onClick={() => navigate('/')}>
+                Home
+              </Button>
+              <Button color="inherit" onClick={() => navigate('/profile')}>
+                Ver Perfil
+              </Button>
+            </Box>
           )}
         </Toolbar>
       </Container>
