c75f128fe3001cac27f4e760fc3fdde7f37bacc8
ivanpfaab <pfaabivan@gmail.com>
2024-07-07 12:33:53 -0300

    Feat: Removing home button and adding it into the FIUBA logo

--- fronted-assignment-service/src/components/Header.js
+++ fronted-assignment-service/src/components/Header.js
@@ -4,18 +4,18 @@ import { useNavigate } from 'react-router-dom';
 import fiubaLogo from '../assets/Logo-fiuba_big_face.png'; 
 
 const Header = ({ user, color, handleHomeClick }) => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleHome = () => {
-    handleHomeClick();  // Llamar la función para restablecer el estado
-    navigate('/');
-  };
+    const handleLogoClick = () => {
+        handleHomeClick();  // Llamar la función para restablecer el estado
+        navigate('/');
+    };
 
   return (
     <AppBar position="static" style={{ backgroundColor: color }}>
       <Container maxWidth="lg">
         <Toolbar>
-          <Box display="flex" alignItems="center" flexGrow={1}>
+        <Box display="flex" alignItems="center" flexGrow={1} onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
             <img src={fiubaLogo} alt="FIUBA Logo" style={{ height: '40px', marginRight: '15px' }} />
             <Typography variant="h6">
               FIUBA
@@ -23,9 +23,6 @@ const Header = ({ user, color, handleHomeClick }) => {
           </Box>
           {user && (
             <Box>
-              <Button color="inherit" onClick={handleHome}>
-                Home
-              </Button>
               <Button color="inherit" onClick={() => navigate('/profile')}>
                 Ver Perfil
               </Button>
