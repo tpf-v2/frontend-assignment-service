9a34a0c0b40816e821ea91186310d622d1e57961
ivanpfaab <pfaabivan@gmail.com>
2024-08-04 15:07:38 -0300

    Refactor for endpoints based on backend

--- fronted-assignment-service/src/api/sendGroupForm.js
+++ fronted-assignment-service/src/api/sendGroupForm.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const sendGroupForm = async (payload) => {
   try {
-    const response = await axios.post('http://127.0.0.1:8000/forms/groups', payload);
+    const response = await axios.post('http://127.0.0.1:8000/forms/answers', payload);
     return response;
   } catch (error) {
     throw new Error('Error al enviar el formulario');
