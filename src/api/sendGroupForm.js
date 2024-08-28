5e5e9c94b702194d0c38e9336c555004ec6620bf
ivanpfaab <pfaabivan@gmail.com>
2024-08-28 18:58:35 -0300

    feat: adjusting group form logic

--- src/api/sendGroupForm.js
+++ src/api/sendGroupForm.js
@@ -8,7 +8,7 @@ export const sendGroupForm = async (payload, existingGroup, user) => {
   };
   try {
     var response;
-    if(!existingGroup){
+    if(existingGroup){
       response = await axios.post('http://127.0.0.1:5000/forms/answers', payload, config);
     }
     else{
