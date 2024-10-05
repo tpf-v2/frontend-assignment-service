bb37b20b7f15f49e4ca3fe276c28e305e331e1f8
celedituro <cdituro@fi.uba.ar>
2024-10-05 10:27:13 -0300

    feat: add date picker inputs

--- src/views/Admin/DashboardView.js
+++ src/views/Admin/DashboardView.js
@@ -19,6 +19,7 @@ import ContentInicio from "../../components/UI/Dashboards/AdminStats/Components/
 import ContentInscripciones from "../../components/UI/Dashboards/AdminStats/Components/ContentInscripciones";
 import ContentAnteproyecto from "../../components/UI/Dashboards/AdminStats/Components/ContentAnteproyecto";
 import Algorithms from "../../components/Algorithms/Algorithms";
+import DatePickerView from "./DatesPickerView";
 
 // Estilos
 const Root = styled(Paper)(({ theme }) => ({
@@ -123,7 +124,7 @@ const Dashboard = () => {
       case "Final":
         return <div>Contenido de entrega Final</div>;
       case "Fechas de presentación":
-        return <div>Contenido del Formulario de Fechas</div>;
+        return <DatePickerView />;
       default:
         return null;
     }
---
+++ src/views/Admin/DatesPickerView.js
@@ -0,0 +1,14 @@
+import DatePickerInput from "../../components/DatePickerInput";
+
+const DatePickerView = () => {
+  
+    return (
+        <div>
+            <DatePickerInput label="Desde"/>
+            <DatePickerInput label="Hasta"/>
+        </div>
+      );
+  };
+  
+  export default DatePickerView;
+  
