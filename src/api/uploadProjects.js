2f6814e6af6b6e6441125cca93886ffb31b28df8
Dario Seminara <dario.seminara@immergo.tv>
2025-06-11 20:33:57 -0300

    #108 utilizar websocket para obtener una notificacion de la tarea de subida al terminar, si no funciona la instanciacion del websocket, se hace fallback a polling rate

--- src/api/uploadProjects.js
+++ src/api/uploadProjects.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import * as Sentry from "@sentry/react";
 
 const BASE_URL = process.env.REACT_APP_API_URL;
+const WS_BASE_URL = process.env.REACT_APP_API_URL
+  .replace("http", "ws")
+  .replace("https", "wss");
 
 export const uploadProjects = async ({ projectType, groupId, projectTitle, selectedFile, url, token }) => {
   const projectNameKeyMap = {
@@ -36,12 +40,54 @@ export const uploadProjects = async ({ projectType, groupId, projectTitle, selec
       });
     }
 
-    if (response.status == 200 && response.data.task_id) {
+    if (response.status == 200 && response.data.task_id && response.data.status == "pending") {
+      try {
+        const result = new Promise((resolve, reject) => {
+          const ws = new WebSocket(`${WS_BASE_URL}/notifications/connect`);
+          ws.onopen = () => {
+            ws.send(JSON.stringify({
+              type: "listen-task",
+              task_id: response.data.task_id
+            }));
+          };
+
+          ws.onerror = (event) => {
+            reject(event);
+          };
+
+          ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            if (data.type == "task-status") {
+              if (data.status == "success") {
+                ws.close();
+                resolve(data);
+              } else {
+                ws.close();
+                reject(data);
+              }
+            }
+          };
+        });
+
+        await result;
+
+        return {
+          success: true,
+          message: `¡Envío exitoso! Se ha registrado correctamente la ${projectNameKeyMap[projectType]}.`,
+        };
+
+      } catch (error) {
+        Sentry.captureException(error);
+        console.error("Error al intentar conectar un websocket", error);
+      }
+
+
+      // Espera a que la tarea se complete mediante polling
       while (true) {
         await new Promise((resolve) => setTimeout(resolve, 1000));
 
         const statusResponse = await axios.get(
-          `${BASE_URL}/groups/${groupId}/${projectType}/status?task_id=${response.data.task_id}`,
+          `${BASE_URL}/tasks/status/${response.data.task_id}`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -62,6 +108,7 @@ export const uploadProjects = async ({ projectType, groupId, projectTitle, selec
       message: `¡Envío exitoso! Se ha registrado correctamente la ${projectNameKeyMap[projectType]}.`,
     };
   } catch (error) {
+    Sentry.captureException(error);
     console.error(`Error al enviar la ${projectNameKeyMap[projectType]}`, error);
     return {
       success: false,
