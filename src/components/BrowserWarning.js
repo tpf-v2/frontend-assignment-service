fefe55867737d0e9fc47b78702ccb7f35b90ca19
Aldana <aldanuchi@gmail.com>
2025-06-18 01:26:42 -0300

    Minor fix a string (ya no es una recomendación sino una nec usar chrome).

--- src/components/BrowserWarning.js
+++ src/components/BrowserWarning.js
@@ -2,7 +2,7 @@ import { Alert } from "@mui/material";
 import { Container } from "@mui/system";
 
 const BrowserWarning = ({ message }) => {
-  message = message || "Para utilizar esta funcionalidad, se recomienda utilizar Google Chrome version 137 o superior."
+  message = message || "Para hacer uso de esta funcionalidad, utilizar Google Chrome (version 137 o superior)."
   return (
     <Container maxWidth="sm">
         <Alert severity="error">
