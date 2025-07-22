59799e23f006252098dfeef8c6774ea2236cfd7e
Dario Seminara <dario.seminara@immergo.tv>
2025-07-22 09:36:17 -0300

    arreglo cambio subido accidentalmente a staging

--- src/services/browserDetect.js
+++ src/services/browserDetect.js
@@ -6,7 +6,7 @@ export default {
     isDateCompatible: () => {
         try {
             const version = browser.browser.version.split('.')[0];
-            return browser.browser.name === 'Chrome' && parseInt(version) >= 0;
+            return browser.browser.name === 'Chrome' && parseInt(version) >= 137;
         } catch (error) {
             console.error('Error al verificar la compatibilidad de navegador', error);
             return false;
