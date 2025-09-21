f4ad16340c322198aa566f52a047fea220cdcb15
Aldana <aldanuchi@gmail.com>
2025-09-21 06:20:20 -0300

    Fix para no alterar comportamiento en estas cards, que se usan en otros componentes

--- src/components/UI/Dashboards/AdminStats/Components/StatCard.js
+++ src/components/UI/Dashboards/AdminStats/Components/StatCard.js
@@ -3,27 +3,34 @@ import { Paper, Typography, CircularProgress } from '@mui/material';
 import { styled } from '@mui/system';
 
 
-const StatCardStyled = styled(Paper)(({ theme, selected }) => ({
-  // La sintaxis de acá arriba indica que propaga las props a Paper, por lo que puede usar onClick
+const StatCardStyled = styled(Paper)(({ theme, selected, onClick }) => ({
+  // (La sintaxis de acá arriba indica que propaga las props a Paper (ej onClick);
+  // para explicitar una debajo como selected u onClick, hay que escribirla acá arriba.=
+
   flex: '1 1 30%',
   margin: theme.spacing(1),
   padding: theme.spacing(2),
   borderRadius: theme.shape.borderRadius,
   boxShadow: theme.shadows[3],
   textAlign: 'center',
-  // Mostrar a la tarjeta como clickeable (sin esto lo es, pero no se nota desde ui)
-  cursor: 'pointer',
-
-  transition: 'box-shadow 0.2s ease-in-out',
-  '&:hover': {
-    boxShadow: theme.shadows[6],
-  },
+  
+  // Lo siguiente solo se aplica si se le pasó onClick, para no cambiar ux en lugares en los que es clickeable
+  ...(onClick && {
+    // Mostrar a la tarjeta como clickeable (sin esto lo es, pero no se nota desde ui)
+    cursor: 'pointer',
+    // Un efecto de sombra por detrás cuando se hace hover
+    transition: 'box-shadow 0.2s ease-in-out',
+    '&:hover': {
+      boxShadow: theme.shadows[7],
+    },
     
-  // Recibo selected y pongo un indicador visual (borde inferior) según su valor
-  borderBottom: selected ? `4px solid #0072C6` : '4px solid transparent',
-  '&:hover': {
-    boxShadow: theme.shadows[6],
-  },
+    // Recibo selected y pongo un indicador visual (borde inferior) según su valor
+    borderBottom: selected ? `4px solid #0072C6` : '4px solid transparent',
+    '&:hover': {
+      boxShadow: theme.shadows[6],
+    },
+  }),    
+  
 }));
 
 
