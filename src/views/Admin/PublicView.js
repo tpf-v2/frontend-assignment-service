ae971105662696a3124427acc88c9557974ee6a2
Maferep <mf.pont.tv@gmail.com>
2025-09-23 20:43:04 -0300

    use const  + cleanup

--- src/views/Admin/PublicView.js
+++ src/views/Admin/PublicView.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { styled } from "@mui/system";
-import { useNavigate, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { Container, Box, Grid, Paper } from "@mui/material";
 import { downloadProject, getPublicProjects } from "../../api/handleProjects";
 import ContentPublicPdfProjects from "../../components/UI/Dashboards/AdminStats/Components/ContentPublicProjectPDFs";
@@ -16,59 +16,58 @@ const Root = styled(Paper)(({ theme }) => ({
 }));
 
 const PublicPDFView = () => {
-  const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-  const [period, setPeriod] = useState(useParams().period);
+  const period = useParams().period;
   const [loadingFinalProjects, setLoadingFinalProjects] = useState(true);
   const [deliveries, setDeliveries] = useState(null);
 
 
-useEffect(() => {
-  const start = async () => {
-    setLoadingFinalProjects(true);
-    const finalProjectsData = await getPublicProjects(
-      user,
-      period,
-      'final'
-    );
-    if (finalProjectsData) {
-      setDeliveries(finalProjectsData);
-    } else {
-      console.error("No se encontraron datos de entregas finales");
-    }
-    setLoadingFinalProjects(false);
-  };
-  start()
-  }, []);
+  useEffect(() => {
+    const start = async () => {
+      setLoadingFinalProjects(true);
+      const finalProjectsData = await getPublicProjects(
+        user,
+        period,
+        'final'
+      );
+      if (finalProjectsData) {
+        setDeliveries(finalProjectsData);
+      } else {
+        console.error("No se encontraron datos de entregas finales");
+      }
+      setLoadingFinalProjects(false);
+    };
+    start()
+    }, []);
 
-  const downloadFinalFile = async (groupId, groupNumber) => {
-    try {
-      await downloadProject(groupId, user, period, "final", groupNumber);
-    } catch (error) {
-      console.error("Error al descargar el archivo:", error);
-    }
-  };
+    const downloadFinalFile = async (groupId, groupNumber) => {
+      try {
+        await downloadProject(groupId, user, period, "final", groupNumber);
+      } catch (error) {
+        console.error("Error al descargar el archivo:", error);
+      }
+    };
 
-  const renderContent = () => {
+    const renderContent = () => {
+      return (
+        <ContentPublicPdfProjects
+          loadingProjects={loadingFinalProjects}
+          deliveries={deliveries}
+          downloadFile={downloadFinalFile}
+          projectType={"final"}
+        />
+      );
+    };
+
     return (
-      <ContentPublicPdfProjects
-        loadingProjects={loadingFinalProjects}
-        deliveries={deliveries}
-        downloadFile={downloadFinalFile}
-        projectType={"final"}
-      />
-    );
-  };
-
-  return (
-    <Container
-      maxWidth={false}
-      sx={{
-        width: "95%", // Ajusta el ancho al 90% del viewport
-        height: "120vh", // Ocupa el 100% de la altura de la pantalla
-        maxWidth: "none", // Para que el maxWidth no limite el tamaño
-      }}
-    >
+      <Container
+        maxWidth={false}
+        sx={{
+          width: "95%", // Ajusta el ancho al 90% del viewport
+          height: "120vh", // Ocupa el 100% de la altura de la pantalla
+          maxWidth: "none", // Para que el maxWidth no limite el tamaño
+        }}
+      >
       <Root>
         <Grid container spacing={3}>
           {/* Sidebar */}
