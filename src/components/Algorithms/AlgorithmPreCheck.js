f4413aaaa87d06d463da44362c1df55f4138d191
Aldana <aldanuchi@gmail.com>
2025-09-27 04:17:50 -0300

    Fix componente verif previa para ambos casos, post adapt

--- src/components/Algorithms/AlgorithmPreCheck.js
+++ src/components/Algorithms/AlgorithmPreCheck.js
@@ -8,25 +8,29 @@ const AlgorithmPreCheck = ({initialDescription, inputInfo, algorithm}) => {
 
   let msg;
   let showWhoComponent;
+  let showWhoList;
   switch (algorithm) {
     case "IncompleteTeams": {
       msg = inputInfo.length === 0 ? "Todos/as los/as estudiantes forman parte de alguna respuesta al formulario."
       : inputInfo.length === 1 ? "Existe 1 estudiante que no está en respuestas al formulario de equipos en ninguna de sus variantes."
       : `Existen ${inputInfo.length} estudiantes que no están en respuestas al formulario de equipos en ninguna de sus variantes.`
 
-      showWhoComponent = <StudentsTable dataListToRender={inputInfo} />;
+      showWhoList = inputInfo;
+      showWhoComponent = <StudentsTable dataListToRender={showWhoList} />;
+
       break;
     }
     case "Dates": {
-
+      
       msg = inputInfo.admin_slots ? (
         inputInfo.teams?.length === 0 ? "Todos los equipos completaron su disponibilidad."
-      : inputInfo.teams?.length === 1 ? "Existe 1 equipo que no completó su disponibilidad."
-      : `Existen ${inputInfo.teams?.length} equipos que no completaron su disponibilidad.`
+        : inputInfo.teams?.length === 1 ? "Existe 1 equipo que no completó su disponibilidad."
+        : `Existen ${inputInfo.teams?.length} equipos que no completaron su disponibilidad.`
       ) : "Primero se debe cargar las fechas disponibles desde la sección Disponibilidad Fechas de Presentación."
-
-      showWhoComponent = <TeamsTable dataListToRender={inputInfo.teams} />;
-
+      
+      showWhoList = inputInfo.teams;
+      showWhoComponent = <TeamsTable dataListToRender={showWhoList} />;      
+      
       break;
     }
     default: {
@@ -58,7 +62,7 @@ const AlgorithmPreCheck = ({initialDescription, inputInfo, algorithm}) => {
         </Typography>
       </Grid>      
 
-      {inputInfo?.teams?.length > 0 && (<Grid item xs={12} md={12} sx={{ display: "flex" }}>  
+      {showWhoList.length > 0 && (<Grid item xs={12} md={12} sx={{ display: "flex" }}>  
         {showWhoComponent}
       </Grid>)}
       </>
