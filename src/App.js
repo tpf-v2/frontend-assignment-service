00015b66addf8061cb5b60e2de84f1a4177b004b
JoaquinHetrea <joahetrea@gmail.com>
2025-10-07 00:31:20 -0300

    fix: arreglar error de scroll y footer

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ import { setStudents } from './redux/slices/studentsSlice';
 import { setTutors } from './redux/slices/tutorsSlice';
 import { setTopics } from './redux/slices/topicsSlice';
 import Credits from './views/CreditsView';
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   const user = useSelector((state) => state.user);
@@ -59,6 +60,7 @@ const App = () => {
 
   return (
     <HashRouter>
+      <ScrollToTop />
       <TokenManager />
       <Box
         className="main-container"
@@ -76,8 +78,6 @@ const App = () => {
           className="content-container"
           sx={{
             flex: '1',
-            minHeight: '1000px',
-            overflowY: 'auto', // Add scrolling if content exceeds
             display: 'flex',
             flexDirection: 'column',
           }}
