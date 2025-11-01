84942d60d76b80e6896d2d5725e6d75fd2419724
Aldana <aldanuchi@gmail.com>
2025-11-01 00:17:27 -0300

    Extraigo el contenido y le aplico estilo según prop, tutores manda false :)
    Queda el título de otro color. En realidad creo que los demás títulos negros son los desfasados, pero bueno, se puede adaptar temporalmente.

--- src/components/Exploration/ExploreIdeas.js
+++ src/components/Exploration/ExploreIdeas.js
@@ -126,93 +126,103 @@ const ExploreIdeas = ({containedStyle=true}) => {
     }
   }
 
+  const content = (
+    <>
+      <Title variant="h5" align="center">Ideas</Title>
+      <Typography>
+      En este espacio se pueden ver las ideas propuestas por estudiantes de este cuatrimestre.
+      Se muestra email de autores de las ideas, para facilitar el contacto para el armado de equipos.
+      Si se obtiene la aprobación de un/a tutor/a, luego, de querer elegirla se debe completar el formulario de equipos
+      indicando la opción "Ya tengo tema y tutor".
+      </Typography>
+      {/* Si no hay ideas */}  
+      {ideas?.length === 0 && (
+        <Alert severity="info" sx={{ mt: 2 }}>
+          Aún no hay ideas propuestas por estudiantes este cuatrimestre.
+        </Alert>
+      )}
+      {/* Botones para estudiantes*/}
+      <Grid item sx={{mb: 2}}>
+        {user.temporal_role === 'student' &&
+          <SubmitButton
+            url="/propose-idea"
+            title="Proponer Idea"
+            width="100%"
+            handleSubmit={() => handleNavigation("/propose-idea")}
+            disabled={team && team.pre_report_date == null} // (si no tienen equipo, da disabled=false, correcto)
+          />
+        }
+        <SubmitButton
+          url="/public"
+          title="Ver proyectos anteriores"
+          width="100%"
+          handleSubmit={() => handleNavigation("/public")}
+          variant='outlined'
+        />
+      </Grid>
+      {/* Renderizado de ideas */}        
+      {ideas?.map((idea) => (
+        <Box key={idea?.id} sx={{ mb: 3, p: 2, border: "1px solid #ccc", borderRadius: 2}}>
+          {/* Botón en mismo renglón que título */}
+          <Box
+            sx={{ 
+              display: 'flex', 
+              gap: 2,
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginBottom: '1rem'                
+            }}
+          >
+            <Typography variant="subtitle1" fontWeight="bold">               
+              {idea?.title}
+            </Typography>
+            
+            {isMyIdea(idea) && (
+              <EditButton
+                onClick={() => {
+                  setEditingIdea(idea);
+                  setOpenEditModal(true)
+                }}
+                sx={{ml: "auto"}} // a la derecha
+              />
+            )}
+          </Box>
+
+          <Typography variant="body1">{idea?.description}</Typography>
+          
+          <Typography variant="body2" mt={1}>
+            Propuesta por: {idea?.student?.name} {idea?.student?.last_name} ({idea?.student?.email})
+          </Typography>
+          <Typography variant="body2">
+            Equipo: {idea?.full_team ? "Completo" : "Aún buscando integrantes"} {""}
+            {isMyIdea(idea) && (
+              <Link
+                component="span"
+                onClick={() => {setEditingIdea(idea); setOpenChangeStatusModal(true)}}
+                underline="always"
+                sx={{ color: "grey", fontWeight: "bold", cursor: "pointer"}}
+              >
+                Cambiar
+              </Link>
+            )}
+          </Typography>
+        </Box>        
+      ))}
+    </>
+  )
+
   return (
     <>
-    <Container maxWidth="md">
-      <Root>
-        <Title variant="h5" align="center">Ideas</Title>
-        <Typography>
-        En este espacio se pueden ver las ideas propuestas por estudiantes de este cuatrimestre.
-        Se muestra email de autores de las ideas, para facilitar el contacto para el armado de equipos.
-        Si se obtiene la aprobación de un/a tutor/a, luego, de querer elegirla se debe completar el formulario de equipos
-        indicando la opción "Ya tengo tema y tutor".
-        </Typography>
-        {/* Si no hay ideas */}  
-        {ideas?.length === 0 && (
-          <Alert severity="info" sx={{ mt: 2 }}>
-            Aún no hay ideas propuestas por estudiantes este cuatrimestre.
-          </Alert>
-        )}
-        {/* Botones para estudiantes*/}
-        <Grid item sx={{mb: 2}}>
-          {user.temporal_role === 'student' &&
-            <SubmitButton
-              url="/propose-idea"
-              title="Proponer Idea"
-              width="100%"
-              handleSubmit={() => handleNavigation("/propose-idea")}
-              disabled={team && team.pre_report_date == null} // (si no tienen equipo, da disabled=false, correcto)
-            />
-          }
-          <SubmitButton
-            url="/public"
-            title="Ver proyectos anteriores"
-            width="100%"
-            handleSubmit={() => handleNavigation("/public")}
-            variant='outlined'
-          />
-        </Grid>
-        {/* Renderizado de ideas */}        
-        {ideas?.map((idea) => (
-          <Box key={idea?.id} sx={{ mb: 3, p: 2, border: "1px solid #ccc", borderRadius: 2}}>
-            {/* Botón en mismo renglón que título */}
-            <Box
-              sx={{ 
-                display: 'flex', 
-                gap: 2,
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                marginBottom: '1rem'                
-              }}
-            >
-              <Typography variant="subtitle1" fontWeight="bold">               
-                {idea?.title}
-              </Typography>
-              
-              {isMyIdea(idea) && (
-                <EditButton
-                  onClick={() => {
-                    setEditingIdea(idea);
-                    setOpenEditModal(true)
-                  }}
-                  sx={{ml: "auto"}} // a la derecha
-                />
-              )}
-            </Box>
-
-            <Typography variant="body1">{idea?.description}</Typography>
-            
-            <Typography variant="body2" mt={1}>
-              Propuesta por: {idea?.student?.name} {idea?.student?.last_name} ({idea?.student?.email})
-            </Typography>
-            <Typography variant="body2">
-              Equipo: {idea?.full_team ? "Completo" : "Aún buscando integrantes"} {""}
-              {isMyIdea(idea) && (
-                <Link
-                  component="span"
-                  onClick={() => {setEditingIdea(idea); setOpenChangeStatusModal(true)}}
-                  underline="always"
-                  sx={{ color: "grey", fontWeight: "bold", cursor: "pointer"}}
-                >
-                  Cambiar
-                </Link>
-              )}
-            </Typography>
-          </Box>        
-        ))}
-                      
-      </Root>
-    </Container>
+      {/* Estilo según prop */}
+      {containedStyle
+        ? (
+          <Container maxWidth="md">
+            <Root>
+              {content}
+            </Root>
+          </Container>
+          ) : content
+      }
       {/* Modals para editar el contenido y el full_team */}
 
       <EditIdeaModal
